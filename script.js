1c067bf1f77e8eb4cc15d9e092d277b0ce9942b2
Mario Ortiz <marioortiz@Marios-MacBook-Pro.local>
2025-04-02 19:53:13 -0700

    The event handler is the actual change that will occur once the event trigger is activated. For example, consider the event listener for a click. It allows you to determine the action to take.Or website changes take effect.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+
+// this sets the buttonchange variable to the button with the id of clickMe
+
 let buttonChange = document.getElementById("clickMe");
+// this checks html and listens or checks for a click event on the buttonchange variable which is the button with clickMe ID
 
 buttonChange.addEventListener("click", function() {
+    // this changes the value of buttonchange variable which is the button with the id of clickMe in the HTML to You clicked me!
+
   buttonChange.innerText = "You clicked me!";
 });
+
+
+
+function getRandomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`; 
+}
+
+document.getElementById("randomColorBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = getRandomColor();
+});
+
